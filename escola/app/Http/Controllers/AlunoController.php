2a246bc93aafa27dca39afe2bb0ec6cb94d579c5
Maicon Lima dos santos <maiconlimadossantos8@gmail.com>
2026-03-06 21:54:59 -0300

    06/03/2026

--- escola/app/Http/Controllers/AlunoController.php
+++ escola/app/Http/Controllers/AlunoController.php
@@ -20,4 +20,39 @@ class AlunoController extends Controller
 
         return redirect()->route('Cadastra_aluno')->with('success', 'Aluno cadastrado com sucesso!');
     }
+    public function calcularIdade($data_nascimento) {
+        $data_nascimento = new \DateTime($data_nascimento);
+        $data_atual = new \DateTime();
+        $idade = $data_atual->diff($data_nascimento)->y;
+        return $idade;
+    }
+
+    public function isPrimo($numero) {
+        if ($numero <= 1) {
+            return false;
+        }
+        for ($i = 2; $i <= sqrt($numero); $i++) {
+            if ($numero % $i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function exibirAluno(Request $request) {
+        $nome = $request->input('nome');
+        $data_nascimento = $request->input('data_nascimento');
+        $idade = $this->calcularIdade($data_nascimento);
+        $isPrimo = $this->isPrimo($idade);
+
+        return view('exibir_aluno', compact('nome', 'idade', 'isPrimo'));
+    }
 }
+
+//O nome do curso em caixa alta (maiúsculas), centralizado e com destaque no topo
+//da página.
+///- A idade calculada do aluno.
+//- O nome do aluno repetido X vezes, onde X é a sua idade calculada.
+//- Regra Especial: Caso a idade do usuário seja um número primo, esses nomes
+//repetidos devem aparecer obrigatoriamente formatados em negrito e com fonte
+//grande.
